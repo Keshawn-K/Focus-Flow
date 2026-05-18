0be7af2ea3309e62ea6b25efd35e0915437530a9
Keshawn-K <keshawn.kingori@student.moringaschool.com>
2026-05-18 08:52:52 +0300

    Fixing environment variables for deployment

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Get environment variables from .env
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = "https://oyphynqwntvgmzsslskv.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95cGh5bnF3bnR2Z216c3Nsc2t2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5Mjg1NjIsImV4cCI6MjA5NDUwNDU2Mn0.3Xmuq4APAEI-11qGbOiz3K3UDgFLcYJsO19HuHiJH44"
 
 // Create and export Supabase client
 export const supabase = createClient(
